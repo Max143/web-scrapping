143f8b12c5815df82850b063137a07797199c13f
Manish <aoen143@gmail.com>
2019-01-28 12:10:07 +0530

    dataquestblogs.py file edited

--- DataquestBlogs.py
+++ DataquestBlogs.py
@@ -1,68 +1,83 @@
-# Data Need From Dataquest Website
-# Title/link/date/tags
+# Task 1: Get Aticle Title, Tag, link, summary    -------------------------Done
+# Task 2: Count the number of article present---------------------------Done
+# Task 3: Get all the tag at one place------------------------------------Done
 
 
 import requests
 from bs4 import BeautifulSoup
+import csv
+from csv import writer
 
 url = 'https://www.dataquest.io/blog/tag/python/'
 
-site = requests.get(url)
+site = requests.get(url).text
 
-soup = BeautifulSoup(site.content, 'html.parser')
+soup = BeautifulSoup(site, 'lxml')
 
-count = 0
+csv_file = open('cms_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
 
-print("Title: ", soup.title.string)
+# writing Columns header
+csv_writer.writerow(['BLOG TITLE', 'TAG', 'SUMMARY', 'LINK'])
 
-post_feed  = soup.find('div', class_='post-feed')
+print("Website Title: ",soup.title.text)
 
-posts = post_feed.find_all('article', "post-card")
-for post in posts:
-    # Blogs Title
-    title = post.find('h2', class_='post-card-title')
-    print("Blog Title: ", title.string)
-    # Blogs Links
-    link = post.find('a', class_='post-card-content-link') 
-    print("Link:  https://www.dataquest.io",link.get('href'))   
-    # Published Date
-    date = post.find('span', class_='post-card-date')
-    print("Published Date: ",date.string)
-    # Tags
-    tags = post.find('span', class_='post-card-tags')
-    print("Tags: ",tags.string) 
+print()
+total_blogs = 0
+all_tag = []
+
+
+content = soup.find('div',class_='site-wrapper').find('main', class_='site-main outer').find('div', class_='inner')
+posts = content.find('div', class_='post-feed')
+
+articles = posts.find_all('article')
+
+for article in articles:
     
-    count += 1
-    print("--------------------------------------------------------------------------------------")
+    # Headline
+    headline = article.h2.text
+    print("Article Headline: ",headline)
+    
+    # Tag
+    tags = article.find('span', class_='post-card-tags').text
+    print("Article Tag: ", tags)
+    if tags not in all_tag:
+        all_tag.append(tags)
 
-print("Total Blogs: ", count)
-''''
-Testing as a single object -
-# I can scrape single data easily 
-single_post = posts[0]
+    # Summary
+    summary = article.find('section').p.text
+    print("Atrticle Summary: ", summary)
 
-title = single_post.find('h2', class_='post-card-title')
-print(title.string)
+    # Link
+    try:
+        broken_link = article.find('a' ,class_='post-card-image-link')['href']
+    except Exception as e:
+        pass
+        
+    # Now, joining the broken link 
+    link = ''.join(["https://www.dataquest.io", broken_link])  # To get the correct links
+    print("Artilce Link: ", link)
 
-date = single_post.find('span', class_='post-card-date')
-print(date.string)
+    total_blogs += 1
+    print()
 
-tags = single_post.find('span', class_='post-card-tags')
-print(tags.string)
+print("-----------------------------------------")
 
-'''
 
+# Total Blogs
+print("Total Blogs: ", total_blogs)
+
+# All tags
+for tag in all_tag:
+    print(tag) 
 
     
+print("Saving Data into well-formated into CSV file")
 
-
-
-
-
-
- 
-
-
-
-
+csv_writer.writerow([headline, tags, summary, link])
+csv_file.close()
+    
+# print(csv_writer)  Do not print becoz it will only generate position of csv_file in memory
+    
+    
 
